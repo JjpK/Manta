ff8f5717cd738039986438b080d4aee623cab299
luoweiwi <luoweiwi@163.com>
2020-03-21 19:07:53 +0800

    U update

---
+++ dist/precache-manifest.f16b3ff5d06d5a3ba26fa3072a9aff36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed2be02812d9f4fbc58084ab961bb77d",
+    "url": "index.html"
+  },
+  {
+    "revision": "d9f9da86fac2a7582bb4237b622d1d25",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "70647387c7835907a532",
+    "url": "static/css/app.8c5894f8.css"
+  },
+  {
+    "revision": "70647387c7835907a532",
+    "url": "static/js/app.b15edcf3.js"
+  },
+  {
+    "revision": "f08efa994ae3a03ac5f9",
+    "url": "static/js/chunk-vendors.5c3abb98.js"
+  }
+]);
