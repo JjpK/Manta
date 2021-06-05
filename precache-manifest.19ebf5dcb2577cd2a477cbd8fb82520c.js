a905d6f364a3ffb1dd7683037d3629f79ef2fc01
JjpK <luoweiwi@163.com>
2021-06-05 23:55:32 +0000

    Automatically update from travis-ci

---
+++ precache-manifest.19ebf5dcb2577cd2a477cbd8fb82520c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe19328c6982a92b3af7c51590d04527",
+    "url": "index.html"
+  },
+  {
+    "revision": "78fd1429420abb23302f74965f9b72ed",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "c9f799e337b30230562a",
+    "url": "static/css/app.0d847831.css"
+  },
+  {
+    "revision": "c9f799e337b30230562a",
+    "url": "static/js/app.831aceeb.js"
+  },
+  {
+    "revision": "df4e93f38b45c15cba6c",
+    "url": "static/js/chunk-vendors.21c9f185.js"
+  }
+]);
